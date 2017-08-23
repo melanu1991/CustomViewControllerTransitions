0680aa93d0f8e15f4ed0940c639a69f3e8952396
melanu1991 <melanu284733@gmail.com>
2017-08-23 19:39:09 +0300

    create project

---
+++ CustomViewControllerTransitions/CustomViewControllerTransitions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomViewControllerTransitions
+//
+//  Created by melanu1991 on 23.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomViewControllerTransitions/CustomViewControllerTransitions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomViewControllerTransitions
+//
+//  Created by melanu1991 on 23.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
